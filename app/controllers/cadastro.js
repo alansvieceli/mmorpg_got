c49ca81ddd9859a1e0e1961438a8f949ec8267cf
Alan Vieceli <alansvieceli@gmail.com>
2019-08-09 13:17:18 -0300

    enviando e recebendo dados

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = (application, req, res) => {
-    res.render("cadastro");res.render("cadastro");
+    res.render("cadastro", {validacao: {}, dadosForm: {}});
+}
+
+module.exports.cadastrar = (application, req, res) => {
+    var dadosForm = req.body;
+
+    req.assert("nome", "Nome não pode ser vazio").notEmpty();
+    req.assert("usuario", "Usuario não pode ser vazio").notEmpty();
+    req.assert("senha", "Senha não pode ser vazio").notEmpty();
+    req.assert("casa", "Casa não pode ser vazio").notEmpty();
+
+    var erros = req.validationErrors();
+    if (erros){        
+        console.log(dadosForm)
+        res.render("cadastro", {validacao: erros, dadosForm: dadosForm})
+        return;
+    }
+
+    res.send("sem erros");
 }
