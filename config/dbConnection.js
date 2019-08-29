c616d526296b12fb2dc115e99915ac988b19141f
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-08-29 15:01:55 -0300

    Configurando conexão com mongoDB

---
+++ config/dbConnection.js
@@ -0,0 +1,19 @@
+var mongodb = require("mongodb");
+
+var connMongoDB = function (){
+  var db = new mongodb.Db(
+    "got",
+    new mongodb.Server(
+      "localhost",
+      27017,
+      {}
+    ),
+    {}
+  );
+
+  return db;
+}
+
+module.exports = function(){
+  return connMongoDB;
+}
